3c0475653d1d5208f4b3f1b2571fdc8e4e55385e
Alperin-Sheriff <jma7@PN105746.campus.nist.gov>
2019-09-07 18:03:14 -0400

    more commitments

--- dist/pdf.worker.js
+++ dist/pdf.worker.js
@@ -123,8 +123,8 @@ return /******/ (function(modules) { // webpackBootstrap
 "use strict";
 
 
-var pdfjsVersion = '2.1.135';
-var pdfjsBuild = '70c460ba';
+var pdfjsVersion = '2.1.136';
+var pdfjsBuild = '261da58a';
 
 var pdfjsCoreWorker = __w_pdfjs_require__(1);
 
@@ -377,7 +377,7 @@ var WorkerMessageHandler = {
     var cancelXHRs = null;
     var WorkerTasks = [];
     var apiVersion = docParams.apiVersion;
-    var workerVersion = '2.1.135';
+    var workerVersion = '2.1.136';
 
     if (apiVersion !== workerVersion) {
       throw new Error("The API version \"".concat(apiVersion, "\" does not match ") + "the Worker version \"".concat(workerVersion, "\"."));
